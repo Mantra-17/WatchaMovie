3614788ef164be1897a6ea8729b57e26b8642504
Patel Mantra <mantrapatel46@gmail.com>
2026-01-13 23:36:28 +0530

    Initial commits

--- src/services/api.js
+++ src/services/api.js
@@ -20,6 +20,11 @@ export const getMovieDetails = async (imdbID) => {
     const response = await fetch(
       `${BASE_URL}?apikey=${API_KEY}&i=${imdbID}&plot=short`
     );
+    
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
     const data = await response.json();
     return data.Response === "True" ? data : null;
   } catch (error) {
